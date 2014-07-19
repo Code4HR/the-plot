5ff983183540cea1b492b38144344e9213edf1ee
Lookmai Rattana <oratt001@gmail.com>
2014-07-19 15:59:21 -0400

    fixed carousel so it doesnt go beyond 0 or 3

--- js/index.js
+++ js/index.js
@@ -1,31 +1,41 @@
 console.log("LOADED");
 $(function(){
+
+	function moveCarousel(index){
+		$(".carousel-item").removeClass("active");
+		$(items[activeItem]).addClass("active");
+
+		$("#carousel").css({
+			"left": (($(window).width() / 2) - ( ($carousel_item_width * index) + ($carousel_item_width/ 2)) )+ "px"
+		});
+	}
+
 	// Stores items in an array
 	var items = [];
 	$("#carousel-item-container .carousel-item").each(function() {
 	    items.push(this);
 	});
 
-	var activeItem = 1;
+	var activeItem = 0;
 
 	$carousel = $("#carousel-item-container");
 	$carousel_item_width = parseInt($(".carousel-item").css("padding-top"));
 
-	$("#carousel").css({
-		"left": (($(window).width() / 2) - ($carousel_item_width/ 2) )+ "px"
-	});
+	moveCarousel(activeItem);
 
 	$(".right").on("click", function(){
-		activeItem += 1;
-		$("#carousel").css({
-			"left": (($(window).width() / 2) - ( ($carousel_item_width * activeItem) + ($carousel_item_width/ 2)) )+ "px"
-		});
+		if (activeItem < 3){
+			activeItem += 1;
+			moveCarousel(activeItem);
+		}
 	})
 
 	$(".left").on("click", function(){
-		activeItem -= 1;
-		$("#carousel").css({
-			"left": (($(window).width() / 2) - ( ($carousel_item_width * activeItem) + ($carousel_item_width/ 2)) )+ "px"
-		});
+		if (activeItem > 0){
+			activeItem -= 1;
+			moveCarousel(activeItem);
+		}
 	})
+
+	
 })
