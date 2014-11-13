bf4f02ec6097a6cd4964a48a41aef18864678aa5
Ann Yuan <ayuan@thisisgrow.com>
2014-11-13 10:08:43 -0500

    mvp

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-The Plot Norfolk
-=====
+THINGS TO KEEP IN MIND
 
-Intro
----
-Landing site for The Plot Norfolk
-
-```
-
-The Plot is a temporary park located in the Downtown Arts District at 776 Granby Street at the corner of Granby Street and Olney Road.
-
-This is the second location for this unique urban recreation space that utilizes reclaimed materials in innovative ways that best utilize the space such as shipping containers used at vertical gardens and public art, a life size chess game and an area dedicated to performance art.
-
-The Plot in the Arts District is scheduled to be complete by the end of summer 2014. Click here to see plans and renderings.
-
-```
+- responsiveness
--- js/index.js
+++ js/index.js
@@ -1,66 +1,7 @@
-console.log("LOADED");
-$(function(){
+function floorAnimate() {
+  document.querySelector("body").className = "rotated";
+}
 
-	function moveCarousel(index){
-		$(".carousel-item").removeClass("active");
-		$(items[activeItem]).addClass("active");
-
-		$("#carousel").animate({
-			"left": (($(window).width() / 2) - ( ($carousel_item_width * index) + ($carousel_item_width/ 2)) )+ "px"
-		});
-	}
-
-	// Stores items in an array
-	var items = [];
-	$("#carousel-item-container .carousel-item").each(function() {
-	    items.push(this);
-	    $(this).find('h1').css({
-	    	"margin-top": ($(".carousel-item-contents").width() / 2 - 20)+ "px"
-	    })
-
-	    $(this).find('h2').css({
-	    	"margin-top": ($(".carousel-item-contents").width() / 2 - 60)+ "px"
-	    })
-	});
-
-	var activeItem = 0;
-
-	$carousel = $("#carousel-item-container");
-	$carousel_item_width = parseInt($(".carousel-item").css("padding-top"));
-
-	// init position
-	$("#carousel").css({
-		"left": (($(window).width() / 2) - ( ($carousel_item_width * 0) + ($carousel_item_width/ 2)) )+ "px"
-	});
-
-	$(".right").on("click", function(){
-		if (activeItem < 3){
-			activeItem += 1;
-			moveCarousel(activeItem);
-		}
-	})
-
-	$(".left").on("click", function(){
-		if (activeItem > 0){
-			activeItem -= 1;
-			moveCarousel(activeItem);
-		}
-	})
-
-	$("#about").on("click", function(){
-		$("#overlay").show();
-		$("#about_box").show();
-	});
-
-	$(".close_btn").on("click", function(){
-		$("#overlay").hide();
-		$("#about_box").hide();
-	});
-
-	$("#overlay").on("click", function(){
-		$("#overlay").hide();
-		$("#about_box").hide();
-	});
-
-	
-})
+function floorUnanimate() {
+  document.querySelector("body").className = "";
+}
