d5372f7b46fa71947b2a35ba53943e755b5deb72
Ann Yuan <ayuan@thisisgrow.com>
2014-11-18 13:38:27 -0500

    reorganization

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ function floorUnanimate() {
   document.querySelector("body").className = "";
 }
 
-var test = false;
-
 window.onload = function() {
 	['top', 'second', 'third', 'fourth'].forEach(function(floor) {
 		var xhr = new XMLHttpRequest();
@@ -15,41 +13,6 @@ window.onload = function() {
 		xhr.onreadystatechange = function() {
 			if(xhr.readyState == 4) {
 				var response = xhr.responseXML.documentElement;
-				if(test && floor == "top") {
-					document.querySelector("html").className = "testing";
-					
-					// =========
-					
-					document.querySelector("#test_1 svg").appendChild(response);
-					document.querySelector("#test_1 svg").setAttribute("width", document.querySelector("#test_1 svg").offsetWidth);
-					document.querySelector("#test_1 svg").setAttribute("height", document.querySelector("#test_1 svg").offsetHeight);
-					document.querySelector("#test_1 svg").setAttribute("viewBox", "0 0 " + document.querySelector("#test_1 svg").offsetWidth + " " + document.querySelector("#test_1 svg").offsetHeight);
-					document.querySelector("#test_1").innerHTML = document.querySelector("#test_1").innerHTML;
-					
-					// =========
-					
-					document.querySelector("#test_2 svg").appendChild(response);
-					document.querySelector("#test_2 svg").setAttribute("width", document.querySelector("#test_2 svg").offsetWidth);
-					document.querySelector("#test_2 svg").setAttribute("height", document.querySelector("#test_2 svg").offsetHeight);
-					document.querySelector("#test_2 svg").setAttribute("viewBox", "0 0 " + document.querySelector("#test_2 svg").offsetWidth + " " + document.querySelector("#test_2 svg").offsetHeight);
-					document.querySelector("#test_2").innerHTML = document.querySelector("#test_2").innerHTML;
-
-					// =========
-					
-					document.querySelector("#test_3 svg").appendChild(response);
-					document.querySelector("#test_3 svg").setAttribute("width", document.querySelector("#test_3 svg").offsetWidth);
-					document.querySelector("#test_3 svg").setAttribute("height", document.querySelector("#test_3 svg").offsetHeight);
-					document.querySelector("#test_3 svg").setAttribute("viewBox", "0 0 " + document.querySelector("#test_3 svg").offsetWidth + " " + document.querySelector("#test_3 svg").offsetHeight);
-					document.querySelector("#test_3").innerHTML = document.querySelector("#test_3").innerHTML;
-
-					// =========
-					
-					document.querySelector("#test_4 svg").appendChild(response);
-					document.querySelector("#test_4 svg").setAttribute("width", document.querySelector("#test_4 svg").offsetWidth);
-					document.querySelector("#test_4 svg").setAttribute("height", document.querySelector("#test_4 svg").offsetHeight);
-					document.querySelector("#test_4 svg").setAttribute("viewBox", "0 0 " + document.querySelector("#test_4 svg").offsetWidth + " " + document.querySelector("#test_4 svg").offsetHeight);
-					document.querySelector("#test_4").innerHTML = document.querySelector("#test_4").innerHTML;
-				}
 
 				document.querySelector("#" + floor + "_floor svg").appendChild(response);
 				document.querySelector("#" + floor + "_floor svg").setAttribute("viewBox", "0 0 " + document.querySelector("#" + floor + "_floor .facade").offsetWidth + " " + document.querySelector("#" + floor + "_floor .facade").offsetHeight);
