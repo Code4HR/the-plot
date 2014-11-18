a27306d38f935b4596880b7743f5918a901c34a4
Ann Yuan <ayuan@thisisgrow.com>
2014-11-18 11:21:47 -0500

    encrispen

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ window.onload = function() {
 				}
 
 				document.querySelector("#" + floor + "_floor svg").appendChild(response);
-				document.querySelector("#" + floor + "_floor svg").setAttribute("viewBox", "0 0 " + document.querySelector("#" + floor + "_floor .facade").offsetHeight + " " + document.querySelector("#" + floor + "_floor .facade").offsetWidth);
+				document.querySelector("#" + floor + "_floor svg").setAttribute("viewBox", "0 0 " + document.querySelector("#" + floor + "_floor .facade").offsetWidth + " " + document.querySelector("#" + floor + "_floor .facade").offsetHeight);
 				document.querySelector("#" + floor + "_floor").innerHTML = document.querySelector("#" + floor + "_floor").innerHTML;
 			}
 		};
